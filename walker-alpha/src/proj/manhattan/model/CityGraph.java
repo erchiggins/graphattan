5afcf5ff5e618aa2e433754d5ecf40f756454c50
erchiggins <emilyrchiggins@gmail.com>
2019-09-25 08:54:48 -0400

    removed cardinal directions and made progress toward adjacency list representation of streets

---
+++ walker-alpha/src/proj/manhattan/model/CityGraph.java
@@ -0,0 +1,21 @@
+package proj.manhattan.model;
+
+import java.util.Map;
+
+public class CityGraph {
+	
+	private Map<Vertex, Map<Vertex, Edge>> corners;
+
+	public Map<Vertex, Map<Vertex, Edge>> getCorners() {
+		return corners;
+	}
+	
+	public void addVertex(Vertex vertex) {
+		
+	}
+	
+	public Map<Vertex, Edge> getAdjacent(Vertex vertex) {
+		return corners.get(vertex);
+	}
+	
+}
